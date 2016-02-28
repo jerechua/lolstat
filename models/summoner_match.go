57bf01b1873227151c9164ba2dce18e58907de48
jeremy <jere.chua@gmail.com>
2016-02-28 14:16:16 -0800

    update tag separator to a space

--- models/summoner_match.go
+++ models/summoner_match.go
@@ -9,13 +9,13 @@ func init() {
 }
 
 type SummonerMatch struct {
-	SummonerID int64  `sql:"unique_index:idx_summid_matchid";validate:"required"`
+	SummonerID int64  `sql:"unique_index:idx_summid_matchid" validate:"required"`
 	Timestamp  int64  `json:"timestamp"`
 	Champion   int    `json:"champion"`
 	Region     string `json:"region"`
 	Queue      string `json:"queue"`
 	Season     string `json:"season"`
-	MatchID    int64  `json:"matchId";sql:"unique_index:idx_summid_matchid";validate:"required"`
+	MatchID    int64  `json:"matchId" sql:"unique_index:idx_summid_matchid" validate:"required"`
 	// Role is either DUO, NONE, SOLO, DUO_CARRY, DUO_SUPPORT
 	Role       string `json:"role"`
 	PlatformID string `json:"platformId"`
