8c035f33ec28018c70035d56cab01e724b3f6921
jeremy <jere.chua@gmail.com>
2016-03-02 23:37:44 -0800

    Move getOrCreateMatchByID to cached client's MatchByID

--- client/riot/cached_riot.go
+++ client/riot/cached_riot.go
@@ -1,7 +1,14 @@
 package riot
 
 import (
+	"fmt"
+	"log"
+
+	"../../db"
 	"../../models"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // cachchedRiotAPI implements RiotAPI with the addition of database caching.
@@ -21,25 +28,49 @@ func newCachcedClient(region string) (*cachedRiotAPI, error) {
 	return &cachedRiotAPI{client}, nil
 }
 
-func (c *cachedRiotAPI) Champions() []*Champion {
-	return c.client.Champions()
+func (api *cachedRiotAPI) Champions() []*Champion {
+	return api.client.Champions()
 }
-func (c *cachedRiotAPI) ChampionByID(ID int) *Champion {
-	return c.client.ChampionByID(ID)
-}
-
-func (c *cachedRiotAPI) SummonersByName(names ...string) ([]Summoner, error) {
-	return c.client.SummonersByName(names...)
+func (api *cachedRiotAPI) ChampionByID(ID int) *Champion {
+	return api.client.ChampionByID(ID)
 }
 
-func (c *cachedRiotAPI) MatchListForSummonerID(ID int64) ([]*models.SummonerMatch, error) {
-	return c.client.MatchListForSummonerID(ID)
+func (api *cachedRiotAPI) SummonersByName(names ...string) ([]Summoner, error) {
+	return api.client.SummonersByName(names...)
 }
 
-func (c *cachedRiotAPI) MatchListSinceTime(ID, beginTime int64) ([]*models.SummonerMatch, error) {
-	return c.client.MatchListSinceTime(ID, beginTime)
+func (api *cachedRiotAPI) MatchListForSummonerID(ID int64) ([]*models.SummonerMatch, error) {
+	return api.client.MatchListForSummonerID(ID)
 }
 
-func (c *cachedRiotAPI) MatchByID(ID int64) (*models.Match, error) {
-	return c.client.MatchByID(ID)
+func (api *cachedRiotAPI) MatchListSinceTime(ID, beginTime int64) ([]*models.SummonerMatch, error) {
+	return api.client.MatchListSinceTime(ID, beginTime)
+}
+
+func (api *cachedRiotAPI) MatchByID(matchID int64) (*models.Match, error) {
+	c := db.MATCH.C()
+	var m *models.Match
+	err := c.Find(&models.Match{MatchID: matchID}).One(&m)
+	switch err {
+	case nil:
+		log.Printf("MatchID: %d found in datastore", matchID)
+		return m, nil
+	case mgo.ErrNotFound:
+		log.Printf("Match ID: %d is not in datastore, fetching from Riot", matchID)
+
+		match, err := api.client.MatchByID(matchID)
+		if err != nil {
+			return nil, err
+		}
+		if match.MatchID != matchID {
+			return nil, fmt.Errorf("Match ID is not the same. Expected ID: %d, but got %d", matchID, match.MatchID)
+		}
+
+		match.ID = bson.NewObjectId()
+		if err := c.Insert(match); err != nil {
+			return nil, err
+		}
+		return match, nil
+	}
+	return nil, err
 }
--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"./client/riot"
 	"./db"
 	"./models"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 func main() {
@@ -54,41 +52,10 @@ func main() {
 			}
 		}
 
-		match, err := getOrCreateMatchDetails(client, m.MatchID)
+		match, err := client.MatchByID(m.MatchID)
 		if err != nil {
 			log.Panic(err)
 		}
 		log.Print(match.MatchID)
-	}
-}
-
-// getOrCreateMatchDetails will try to retrieve the match details from the datastore
-// first. If it doesn't fine it, it will fetch the details directly from the
-// Riot API.
-func getOrCreateMatchDetails(client riot.RiotAPI, matchID int64) (*models.Match, error) {
-	c := db.MATCH.C()
-	var m *models.Match
-	err := c.Find(&models.Match{MatchID: matchID}).One(&m)
-	switch err {
-	case nil:
-		return m, nil
-	case mgo.ErrNotFound:
-		log.Printf("Match ID: %d is not in datastore, fetching from Riot", matchID)
-
-		match, err := client.MatchByID(matchID)
-		if err != nil {
-			return nil, err
-		}
-		if match.MatchID != matchID {
-			return nil, fmt.Errorf("Match ID is not the same. Expected ID: %d, but got %d", matchID, match.MatchID)
-		}
-
-		match.ID = bson.NewObjectId()
-		if err := c.Insert(match); err != nil {
-			return nil, err
-		}
-		return match, nil
-	default:
-		return nil, err
 	}
 }
