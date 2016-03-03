d3f7dd65570259b926ca2d5f081fafa78d8209b6
jeremy <jere.chua@gmail.com>
2016-03-02 23:17:59 -0800

    Added a cached client for riot api

---
+++ client/riot/cached_riot.go
@@ -0,0 +1,41 @@
+package riot
+
+import (
+	"../../models"
+)
+
+// cachchedRiotAPI implements RiotAPI with the addition of database caching.
+type cachedRiotAPI struct {
+	client *riotAPI
+}
+
+func NewCachedNAClient() (RiotAPI, error) {
+	return newCachcedClient("na")
+}
+
+func newCachcedClient(region string) (*cachedRiotAPI, error) {
+	client, err := newClient(region)
+	if err != nil {
+		return nil, err
+	}
+	return &cachedRiotAPI{client}, nil
+}
+
+func (c *cachedRiotAPI) Champions() []*Champion {
+	return c.client.Champions()
+}
+func (c *cachedRiotAPI) ChampionByID(ID int) *Champion {
+	return c.client.ChampionByID(ID)
+}
+
+func (c *cachedRiotAPI) SummonersByName(names ...string) ([]Summoner, error) {
+	return c.client.SummonersByName(names...)
+}
+
+func (c *cachedRiotAPI) MatchListForSummonerID(ID int64) ([]*models.SummonerMatch, error) {
+	return c.client.MatchListForSummonerID(ID)
+}
+
+func (c *cachedRiotAPI) MatchByID(ID int64) (*models.Match, error) {
+	return c.client.MatchByID(ID)
+}
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	db.Init()
 
-	client, err := riot.NAClient()
+	client, err := riot.NewCachedNAClient()
 	if err != nil {
 		log.Panic(err)
 	}
