d1b016745854dbbb3ff7a55567d5d8f56e29afb8
jeremy <jere.chua@gmail.com>
2016-03-06 15:02:06 -0800

    Added body parser and vars to scope

--- gong/scope.go
+++ gong/scope.go
@@ -1,7 +1,11 @@
 package gong
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Scope struct {
@@ -9,6 +13,22 @@ type Scope struct {
 	res http.ResponseWriter
 }
 
+// Write writes to the body of the response.
 func (s *Scope) Write(out []byte) {
 	s.res.Write(out)
+}
+
+// BodyJSON reads the body of the request and parses it into JSON.
+func (s *Scope) BodyJSON(in interface{}) error {
+	decoder := json.NewDecoder(s.req.Body)
+	decoder.UseNumber()
+	if err := decoder.Decode(in); err != nil {
+		return fmt.Errorf("unable to decode post payload")
+	}
+	return nil
+}
+
+// Vars returns the map of URL variables to value.
+func (s *Scope) Vars() map[string]string {
+	return mux.Vars(s.req)
 }
