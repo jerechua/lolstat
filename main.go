216edc596f1e8b2fa45cbfb695d21d62e16f1eb2
jeremy <jere.chua@gmail.com>
2016-03-06 01:41:43 -0800

    Created a Gorilla mux

--- main.go
+++ main.go
@@ -3,61 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
-	"./client/riot"
 	"./db"
-	"./models"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.Init()
 
-	client, err := riot.NewCachedNAClient()
-	if err != nil {
-		log.Panic(err)
-	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", MainHandler)
 
-	for _, c := range client.Champions() {
-		fmt.Printf("id: %d,  name: %s\n", c.ID, c.Name)
-		break
-	}
+	port := 8080
+	log.Printf("Listening on port: %d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+}
 
-	fmt.Println(len(client.Champions()))
-
-	summ, err := client.SummonersByName("blooooop")
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Printf("~~%v\n", summ)
-
-	fmt.Println(client.ChampionByID(99))
-
-	// matches, err := client.MatchListSinceTime(25286733, 1454822389504)
-	matches, err := client.MatchListForSummonerID(25286733)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Fatal(matches)
-	for _, m := range matches {
-		exists, err := db.GORM.Exists(&models.SummonerMatch{
-			MatchID:    m.MatchID,
-			SummonerID: m.SummonerID,
-		})
-		if err != nil {
-			log.Panic(err)
-		}
-		if exists {
-			log.Printf("%d already exists", m.MatchID)
-		} else {
-			if err := db.GORM.Create(m); err != nil {
-				log.Panic(err)
-			}
-		}
-
-		match, err := client.MatchByID(m.MatchID)
-		if err != nil {
-			log.Panic(err)
-		}
-		log.Print(match.MatchID)
-	}
+func MainHandler(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("gorilla!\n"))
 }
