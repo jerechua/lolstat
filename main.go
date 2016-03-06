04932ca042ed3127cdcffd35b02eb9e0b1df3325
jeremy <jere.chua@gmail.com>
2016-03-06 15:55:53 -0800

    Added port flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,13 +13,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	portFlag = flag.Int("port", 8080, "The port to start the server on.")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
 	db.Init()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", gong.Shim(home.MainHandler))
 
-	port := 8080
-	log.Printf("Listening on port: %d", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	log.Printf("Listening on port: %d", *portFlag)
+	http.ListenAndServe(fmt.Sprintf(":%d", *portFlag), r)
 }
